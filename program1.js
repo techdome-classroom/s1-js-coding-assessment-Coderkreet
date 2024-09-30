4335fbfd2502f9d63b940eacc0ce5632334aafa5
Coderkreet <krunalbende2@gmail.com>
2024-09-30 12:26:30 +0530

    Completed ✅

--- program1.js
+++ program1.js
@@ -3,7 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = s[i];
+        if (stack.length) {
+            const last = stack[stack.length - 1];
+            if (isPair(last, cur)) {
+                stack.pop();
+                continue;
+            }
+        }
+        stack.push(cur);
+    }
+
+    return stack.length === 0;  
+};
+
+var isPair = function(last, cur) {
+    return (
+        (last === '(' && cur === ')') ||
+        (last === '{' && cur === '}') ||
+        (last === '[' && cur === ']')
+    );
 };
 
 module.exports = { isValid };
